349524e746afd3a6b4b7cf8573bbfac6966473e6
unknown <avital_boom@hotmail.com>
2018-08-15 11:08:12 +0300

    started the project

--- main.js
+++ main.js
@@ -1,2 +1,44 @@
 //write your code here to make the tests pass
 
+
+class Document {
+    constructor(EmployeeName) {
+        this._EmployeeName = EmployeeName;
+    }
+}
+
+class Employee {
+    constructor(name) {
+        this._name = name;
+    }
+    work() {
+
+    }
+}
+
+class Manager {
+    constructor(name) {
+        this._name = name;
+        this._manageEmployees = []
+    }
+    hireEmployee(employeeName){
+        this._manageEmployees.push(employeeName)
+    }
+    askEmployeesToWork(){
+        
+    }
+
+    
+
+class Cleaner {
+    constructor(name) {
+        this._name = name;
+    }
+    clean() {
+        console.log("Clean");
+    }
+}
+
+class Office {
+
+}
